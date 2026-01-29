3c44eb710e275e9929235c3553807f96d43d78f4
jeevad228 <jeevad4235@gmail.com>
2026-01-29 16:02:17 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class helloworld{
-public static void main(string[] args)
+public static void main(String[] args)
 {
-system.out.println("helloworld");
+System.out.println("helloworld");
 }
 }
